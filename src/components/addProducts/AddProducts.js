ed7397166df46dfb8435c746a70492acfcc216fa
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-11-07 20:12:43 +0500

    Add products ui component created

--- src/components/addProducts/AddProducts.js
+++ src/components/addProducts/AddProducts.js
@@ -1,11 +1,95 @@
-
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AddProducts = () => {
-    return(
-        <div>
-            <h1>Add Products</h1>
+
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState('')
+    const [category, setCategory] = useState('')
+    const [userId, setUserId] = useState('6720e62e43aeb10a5d536b4d') //hardcoding userId for the time being
+    const [company, setCompany] = useState('')
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const auth = localStorage.getItem("user")
+        if(!auth){
+          navigate('/login')
+        }
+      }, [])
+
+    const handleSubmit = async (event) => {
+        console.warn(`name: ${name}`)
+        console.warn(`price: ${price}`)
+        console.warn(`category: ${category}`)
+        console.warn(`userId: ${userId}`)
+        console.warn(`company: ${company}`)
+    }
+    return (
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <div className="card">
+                        <div className="card-header text-center">
+                            <h2>Add Product</h2>
+                        </div>
+                        <div className="card-body">
+                            <form onSubmit={handleSubmit}>
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label">name</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="email"
+                                        placeholder="Enter email"
+                                        value={name}
+                                        onChange={(e) => setName(e.target.value)}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label">price</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="email"
+                                        placeholder="Enter email"
+                                        value={price}
+                                        onChange={(e) => setPrice(e.target.value)}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label">category</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="email"
+                                        placeholder="Enter email"
+                                        value={category}
+                                        onChange={(e) => setCategory(e.target.value)}
+                                    />
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label">company</label>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        id="email"
+                                        placeholder="Enter email"
+                                        value={company}
+                                        onChange={(e) => setCompany(e.target.value)}
+                                    />
+                                </div>
+                               
+                                <div className="d-grid">
+                                    <button type="submit" className="btn btn-primary">Add Product</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
 
-export default AddProducts
+export default AddProducts;
